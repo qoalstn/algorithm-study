f05832f9c66430d7b8cc3f84b38a6056388c144f
qoalstn <mousson2020@gmail.com>
2021-12-03 10:44:25 +0900

    Baekjoon_ 10845 주석

--- Baekjoon/10845_queue/10845_queue.js
+++ Baekjoon/10845_queue/10845_queue.js
@@ -1,8 +1,7 @@
-// import fs from 'fs';
-
 // const fs = require('fs');
 // const input = fs.readFileSync('/dev/stdin/').toString().split('\n');
 
+//'/dev/stdin' 경로 오류로 readline을 사용했다.
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -14,16 +13,16 @@ let input = [];
 rl.on('line', function (line) {
   input.push(line);
 }).on('close', function () {
-  const queue_list = [];
-  let result = '';
+  const queue_list = []; // push로 추가 및 shift로 삭제된 결과가 반영된 배열
+  let result = ''; //출력해야 할 값들의 누적
 
   for (const i in input) {
     switch (true) {
       case input[i].includes('pop'):
         if (queue_list.length == 0) {
-          result += Number(-1) + '\n';
+          result += Number(-1) + '\n'; //-1을 그대로 출력 할 경우 undefined가 나와서 Number로 변환
         } else {
-          result += queue_list.shift() + '\n';
+          result += queue_list.shift() + '\n'; // 첫번째 정수를 빼기 위해shift 함수를 사용
         }
         break;
 
