dcfd9b6f6a9e92c1639d28accfdc5ee0d118faef
qoalstn <mousson2020@gmail.com>
2021-12-17 10:17:20 +0900

    Baekjoon : 10773_스택 (+주석)

--- Baekjoon/stack/10773.js
+++ Baekjoon/stack/10773.js
@@ -9,18 +9,19 @@ let lines = [];
 rl.on('line', function (line) {
   lines.push(line);
 }).on('close', function () {
-  const N = lines[0];
-  let result = 0;
+  const N = lines[0]; // 첫째줄의 입력값은 총 입력값의 갯수
+  let result = 0;//최종 결과 초기화
 
   let tmp = [];
   for (let i = 1; i <= N; i++) {
     if (lines[i] == 0) {
-      tmp.pop();
+      tmp.pop(); // 입력값이 0이면 마지막 값을 빼내고
     } else {
-      tmp.push(lines[i]);
+      tmp.push(lines[i]); // 0이 아닐경우 입력값을 배열에 추가한다.
     }
   }
 
+//주어진 규칙에 따라 만들어진 배열 tmp의 값을 모두 합해 result를 만든다.
   for (const i of tmp) {
     result += Number(i);
   }
