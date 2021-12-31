a43bd3b751e52badd4e0f436aa33345abf1c16b4
qoalstn <mousson2020@gmail.com>
2021-12-31 16:05:10 +0900

    Baekjoon_7568.js

--- Baekjoon/브루트포스/7568.js
+++ Baekjoon/브루트포스/7568.js
@@ -1,21 +1,32 @@
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// let lines = [];
+let lines = [];
 
-// rl.on('line', function (line) {
-//   lines.push(line);
-// }).on('close', function () {
-//   const N = lines.shift();
+rl.on('line', function (line) {
+  lines.push(line);
+}).on('close', function () {
+  const N = lines.shift();
 
-//   for (let i = 0; i < N; i++) {
-//     const x = lines[i].split(' ')[0];
-//     const y = lines[i].split(' ')[1];
-//   }
+  let result = [];
+  for (let i = 0; i < N; i++) {
+    const x = lines[i].split(' ')[0];
+    const y = lines[i].split(' ')[1];
 
-//   console.log(N, lines);
-//   process.exit();
-// });
+    let count = 0;
+    for (let j = 0; j < N; j++) {
+      const x_ = lines[j].split(' ')[0];
+      const y_ = lines[j].split(' ')[1];
+      if (i !== j && x < x_ && y < y_) {
+        count++;
+      }
+    }
+    result.push(count + 1);
+    count = 0;
+  }
+  console.log(result.join(' '));
+  process.exit();
+});
