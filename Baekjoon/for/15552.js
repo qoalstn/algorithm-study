60270f4b31fba92deba7b8c1fdf1f12eff292d83
Seangki Kim <mousson2020@gmail.com>
2022-01-17 22:13:45 +0900

    Baekjoon : 15552_빠른 A+B

--- Baekjoon/for/15552.js
+++ Baekjoon/for/15552.js
@@ -1,22 +1,32 @@
-const readline = require('readline');
+const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-let lines = [];
+let lines = []
 
 rl.on('line', function (line) {
-  lines.push(line);
+  lines.push(line)
 }).on('close', function () {
-  const T = lines.shift();
+  // const T = lines.shift();
 
-  lines.forEach((e) => {
-    // console.log(e.split(' ')[0]);
-    // console.log(e.split(' ')[1]);
-    const result = Number(e.split(' ')[0]) + Number(e.split(' ')[1]);
-    console.log(result);
-  });
+  // lines.forEach((e) => {
+  //   // console.log(e.split(' ')[0]);
+  //   // console.log(e.split(' ')[1]);
+  //   const result = Number(e.split(' ')[0]) + Number(e.split(' ')[1]);
+  //   console.log(result);
+  // });
 
-  process.exit();
-});
+  let max = Number(lines[0])
+  let rs = ''
+
+  for (let i = 1; i <= max; i++) {
+    let num = lines[i].split(' ')
+    rs += Number(num[0]) + Number(num[1]) + '\n'
+  }
+
+  console.log(rs)
+
+  process.exit()
+})
--- Baekjoon/정렬/1427.js
+++ Baekjoon/정렬/1427.js
@@ -1,25 +1,25 @@
-const readline = require('readline');
+const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-let lines = [];
+let lines = []
 
 rl.on('line', function (line) {
-  lines.push(line);
+  lines.push(line)
 }).on('close', function () {
-  const input = lines[0].split(''); // 입력값들을 공백을 기준으로 잘라 배열에 담는다
+  const input = lines[0].split('') // 입력값들을 공백을 기준으로 잘라 배열에 담는다
   const tmp = input.sort((a, b) => {
-    return b - a; // 입력받은 값을 내림차순으로 정렬
-  });
+    return b - a // 입력받은 값을 내림차순으로 정렬
+  })
 
-  let result = '';
+  let result = ''
   for (const i of tmp) {
-    result = result + i; //tmp의 원소들을 문자열로 합친다.
+    result = result + i //tmp의 원소들을 문자열로 합친다.
   }
 
-  console.log(result);
+  console.log(result)
 
-  process.exit();
-});
+  process.exit()
+})
